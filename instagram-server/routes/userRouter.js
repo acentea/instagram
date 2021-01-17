7657bea3778d7609d47f6899e2bf80132f216927
CENTEA Andrei <Andrei.CENTEA@frequentis.com>
2021-01-17 19:56:43 +0200

    Add auth middleware and add routes for "userDelete" and "tokenIsValid"

--- instagram-server/routes/userRouter.js
+++ instagram-server/routes/userRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 const User = require("../models/userModel");
 
 router.post("/register", async (req, res) => {
@@ -57,7 +58,7 @@ router.post("/login", async (req, res) => {
             return res.status(400).json({msg: "No account with this email has been registered"});
         }
 
-        const isMatch = await  bcrypt.compare(password, user.password);
+        const isMatch = await bcrypt.compare(password, user.password);
 
         if (!isMatch) {
             return res.status(400).json({msg: "Invalid credentials"});
@@ -76,8 +77,39 @@ router.post("/login", async (req, res) => {
     } catch (err) {
         return res.status(500).json({error: err.message});
     }
-
 });
 
+router.delete("/delete", auth, async (req, res) => {
+ try {
+     const deletedUser = await User.findByIdAndDelete(req.user);
+     res.json(deletedUser);
+ } catch (err) {
+        return res.status(500).json({error: err.message});
+    }
+});
+
+router.post("/tokenIsValid", async (req, res) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) {
+            return res.json(false);
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) {
+            return res.json(false);
+        }
+
+        const user = await User.findById(verified.id);
+        if (!user) {
+            return res.json(false);
+        }
+
+        return res.json(true);
+
+    } catch (err) {
+        return res.status(500).json({error: err.message});
+    }
+})
 
 module.exports = router;
