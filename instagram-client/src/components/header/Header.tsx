26b534e617cc64208e54b67168ceb92c1a5ad4c2
CENTEA Andrei <Andrei.CENTEA@frequentis.com>
2021-03-08 23:21:25 +0200

    PLX-1001 Create Header buttons

--- instagram-client/src/components/header/Header.tsx
+++ instagram-client/src/components/header/Header.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import instagramImage from './headerIcons/instagram.png';
+import HomeIcon from '@material-ui/icons/Home';
+import MessageIcon from '@material-ui/icons/Message';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import './header.css';
 
 export default function Header() {
     return (
-            <>
-                <div className='header'>
-                    <img src={instagramImage} width='100' height='50' />
+        <>
+            <div className='header'>
+                <div className='header-image'>
+                    <div><img src={instagramImage}/></div>
                 </div>
-            </>
+                <div className='header-buttons'>
+                    <div><HomeIcon/></div>
+                    <div><MessageIcon/></div>
+                    <div><FavoriteIcon/></div>
+                </div>
+            </div>
+        </>
     )
 }
